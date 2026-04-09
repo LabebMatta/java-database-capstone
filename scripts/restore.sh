4045c017a0df2b6c917e52d0679fade2a27ccfb5
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 <labeb.matta@gmail.com>
2026-04-08 21:21:58 -0400

    Completed DTO's, controllers, service classes, and repositories

--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 echo "Restoring MySQL database..."
-mysql -u root -p < /home/project/java-database-capstone/scripts/cms_backup.sql
+mysql -u root -p cms < /home/project/java-database-capstone/scripts/cms_backup.sql
 echo "MySQL database restored successfully!"
 
 echo "Restoring MongoDB database..."
 # Replace 'admin' with the auth database if your user is created there
-mongosh "mongodb://root:XgUjL03qKh9Mjb455RYpgiiA@172.21.120.132:27017/prescriptions?authSource=admin" \
+mongosh "mongodb://root:ZgNe9jW8v5DEQ0DIMxL8SU2L@172.21.135.71:27017/prescriptions?authSource=admin" \
 < /home/project/java-database-capstone/scripts/mongodb_setup.js
 if [ $? -eq 0 ]; then
   echo "MongoDB database restored successfully!"
